69bc0896ffc41b00f9bc2089639c6e69665e5948
agohorel <gohorelalex@gmail.com>
2020-02-18 16:35:33 -0500

    POST route returns newly created car rather than just ID

--- server.js
+++ server.js
@@ -16,10 +16,24 @@ server.get("/cars", async (req, res) => {
 server.post("/cars", async (req, res) => {
   try {
     const newCarID = await db("cars").insert(req.body);
-    res.status(201).json(newCarID);
+    const newCar = await db("cars")
+      .where({ id: Number(newCarID) })
+      .first();
+    res.status(201).json(newCar);
   } catch (error) {
     res.status(500).json({ error: "server error :(" });
   }
 });
+
+// server.put("/cars/:id", async (req, res) => {
+//   try {
+//     const updatedCarID = await db("cars")
+//       .where({ id: req.params.id })
+//       .update(req.body);
+//     res.status(200).json(updatedCarID);
+//   } catch (error) {
+//     res.status(500).json({ error: "server error :(" });
+//   }
+// });
 
 module.exports = server;
